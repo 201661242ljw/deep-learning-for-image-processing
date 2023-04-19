e61a1052041a88dc5f2fd64dc29dccf37d9bf1bb
201661242ljw <mironovasochizuka1998@gmail.com>
2023-04-19 21:34:21 +0800

    init

--- pytorch_classification/swin_transformer/predict.py
+++ pytorch_classification/swin_transformer/predict.py
@@ -11,6 +11,22 @@ from model import swin_tiny_patch4_window7_224 as create_model
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # create model
+    model = create_model(num_classes=2).to(device)
+    # load model weights
+    model_weight_path = "./weights/model-9.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
+
+    # # read class_indict
+    # json_path = './class_indices.json'
+    # assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+    #
+    # with open(json_path, "r") as f:
+    #     class_indict = json.load(f)
+
+    class_indict = {"0": 0,
+                    "1": 1}
 
     img_size = 224
     data_transform = transforms.Compose(
@@ -19,43 +35,72 @@ def main():
          transforms.ToTensor(),
          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
+
     # load image
-    img_path = "../tulip.jpg"
-    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
-    img = Image.open(img_path)
-    plt.imshow(img)
-    # [N, C, H, W]
-    img = data_transform(img)
-    # expand batch dimension
-    img = torch.unsqueeze(img, dim=0)
+    # img_path = "../tulip.jpg"
 
-    # read class_indict
-    json_path = './class_indices.json'
-    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+    s = ""
+    num_00 = 0
+    num_01 = 0
+    num_10 = 0
+    num_11 = 0
 
-    with open(json_path, "r") as f:
-        class_indict = json.load(f)
+    num = 0
 
-    # create model
-    model = create_model(num_classes=5).to(device)
-    # load model weights
-    model_weight_path = "./weights/model-9.pth"
-    model.load_state_dict(torch.load(model_weight_path, map_location=device))
-    model.eval()
-    with torch.no_grad():
-        # predict class
-        output = torch.squeeze(model(img.to(device))).cpu()
-        predict = torch.softmax(output, dim=0)
-        predict_cla = torch.argmax(predict).numpy()
+    src_dir = r"../../partial_imgs/test"
+    for src_dir_1 in os.listdir(src_dir):
+        src_dir_1_path = os.path.join(src_dir, src_dir_1)
+        for file_name in os.listdir(src_dir_1_path):
+            num += 1
+            img_path = os.path.join(src_dir_1_path, file_name)
 
-    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
-                                                 predict[predict_cla].numpy())
-    plt.title(print_res)
-    for i in range(len(predict)):
-        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
-                                                  predict[i].numpy()))
-    plt.show()
+            # img_path = r"E:\LJW\Git\deep-learning-for-image-processing\partial_imgs\test\0\04_1_032_head_no_0_0__1_1_1.JPG"
+            assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+            img = Image.open(img_path)
+            plt.imshow(img)
+            # [N, C, H, W]
+            img = data_transform(img)
+            # expand batch dimension
+            img = torch.unsqueeze(img, dim=0)
 
+            with torch.no_grad():
+                # predict class
+                output = torch.squeeze(model(img.to(device))).cpu()
+                predict = torch.softmax(output, dim=0)
+                predict_cla = torch.argmax(predict).numpy()
+
+                s = s + "{} {} {}\n".format(img_path, src_dir_1, predict_cla)
+
+                if src_dir_1 == "0" and predict_cla == "0":
+                    num_00 += 1
+                if src_dir_1 == "0" and predict_cla == "1":
+                    num_01 += 1
+                if src_dir_1 == "1" and predict_cla == "0":
+                    num_10 += 1
+                if src_dir_1 == "1" and predict_cla == "1":
+                    num_11 += 1
+                print(num, img_path, src_dir_1, predict_cla)
+                #
+                # print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                #                                              predict[predict_cla].numpy())
+                # plt.title(print_res)
+                # for i in range(len(predict)):
+                #     print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                #                                               predict[i].numpy()))
+                # plt.show()
+
+
+    save_path = "results.txt"
+    f = open(save_path, "w", encoding="utf-8")
+    f.write(s)
+    f.close()
+    s2 = "00;{}\n01:{}\n10:{}\n:11:{}".format(num_00, num_01, num_10, num_11)
+
+    save_path = "summary_results.txt"
+    f = open(save_path, "w", encoding="utf-8")
+    f.write(s2)
+    f.close()
 
 if __name__ == '__main__':
     main()
+
